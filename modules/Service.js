19d566eb9480db7c822ff2aaf2eccb05070f89b8
Matheus Moises da Rosa <matheusmoises.rosa@gmail.com>
2018-03-20 16:42:05 -0300

    Ajustando Get e Set

--- modules/Service.js
+++ modules/Service.js
@@ -11,8 +11,38 @@ function Service() {
     var id;
     var descriptionOfService;
     var time;
-    var price;
+    var priceService;
     var client;
     var product;
     var typeOfService;
+
+    /*
+
+   Section of Get and Set
+
+   */
+
+    this.setDescriptionOfService = function (newDescriptionOfService) {
+        descriptionOfService = newDescriptionOfService
+    };
+
+    this.getDescriptionOfService = function () {
+        return descriptionOfService;
+    };
+
+    this.setTime = function (newTime) {
+        time = newTime
+    };
+
+    this.getTime = function () {
+        return time;
+    };
+
+    this.setPriceService = function (newPriceService) {
+        priceService = newPriceService
+    };
+
+    this.getPriceService = function () {
+        return priceService;
+    };
 }
--- modules/User.js
+++ modules/User.js
@@ -13,4 +13,42 @@ function User() {
     var typeOfUser;
     var user;
     var password;
+
+    /*
+
+   Section of Get and Set
+
+   */
+
+    this.setProfile = function (newProfile) {
+        profile = newProfile
+    };
+
+    this.getProfile = function () {
+        return profile;
+    };
+
+    this.setTypeOfUser = function (newTypeOfUser) {
+        typeOfUser = newTypeOfUser
+    };
+
+    this.getTypeOfUser = function () {
+        return typeOfUser;
+    };
+
+    this.setUser = function (newUser) {
+        user = newUser
+    };
+
+    this.getUser = function () {
+        return user;
+    };
+
+    this.setPassword = function (newPassword) {
+        password = newPassword
+    };
+
+    this.getPassword = function () {
+        return password
+    }
 }
